1f2e686a078fbcf140ddaac91a7497fc7df3f1ee
Preensie <smpreen00@gmail.com>
2025-06-28 13:17:21 +0100

    working gitdiff

--- gitdiff.sh
+++ gitdiff.sh
@@ -3,17 +3,21 @@
 # Process to update the local branch automatically
 # To run indefinatly in pm2
 
-git fetch -v --dry-run 2>&1 |
-    grep -qE "\[up\s+to\s+date\]\s+$(
-        git branch 2>/dev/null |
-           sed -n '/^\*/s/^\* //p' |
-                sed -r 's:(\+|\*|\$):\\\1:g'
-    )\s+" || {
-        echo >&2 "Current branch need a 'git pull' before commit"
-        exit 1
+update_branch () {
+	echo "Remote branch has changes, pulling"
+	git pull
 }
 
-update_branch(){
-	echo "Remote branch has changes, pulling"
-	git pull 
-}
+while :
+do
+	git fetch -v --dry-run 2>&1 |
+	 grep -qE "\[up\s+to\s+date\]\s+$(
+       	 git branch 2>/dev/null |
+        	   sed -n '/^\*/s/^\* //p' |
+        	        sed -r 's:(\+|\*|\$):\\\1:g'
+   	 )\s+" || {
+        	update_branch
+		exit 1
+	}
+done
+
